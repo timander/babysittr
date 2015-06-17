43b86dac91e938bb5b5d1fa13f8099122f87b1df
Tim Andersen <tandersen@leantechniques.co>
2015-06-17 12:01:23 -0500

    removing duplication

--- src/main/java/net/timandersen/BabySitterPaymentCalculator.java
+++ src/main/java/net/timandersen/BabySitterPaymentCalculator.java
@@ -9,17 +9,18 @@ public class BabySitterPaymentCalculator {
     private final int STANDARD_RATE = 12;
     private final int AFTER_BEDTIME_RATE = 8;
     private final int AFTER_MIDNIGHT_RATE = 16;
+    private final int MIDNIGHT = 12;
     private int bedTime;
     private final Map<Integer, Integer> hourlyRates = new HashMap<Integer, Integer>();
 
     public BabySitterPaymentCalculator(int bedTime) {
         this.bedTime = adjustTime(bedTime);
         for (int hour = 5; hour <= 16; hour++) {
-            if (beforeBedtime(hour) && beforeMidnight(hour)) {
+            if (after(hour, this.bedTime) && before(hour, MIDNIGHT)) {
                 hourlyRates.put(hour, AFTER_BEDTIME_RATE);
-            } else if (afterBedtime(hour) && beforeMidnight(hour)) {
+            } else if (before(hour, this.bedTime) && before(hour, MIDNIGHT)) {
                 hourlyRates.put(hour, STANDARD_RATE);
-            } else if (afterMidnight(hour)) {
+            } else if (after(hour, MIDNIGHT)) {
                 hourlyRates.put(hour, AFTER_MIDNIGHT_RATE);
             } else {
                 throw new IllegalStateException();
@@ -27,20 +28,12 @@ public class BabySitterPaymentCalculator {
         }
     }
 
-    private boolean afterMidnight(int hour) {
-        return hour >= 12;
+    private boolean after(int hour, int time) {
+        return hour >= time;
     }
 
-    private boolean afterBedtime(int hour) {
-        return hour < this.bedTime;
-    }
-
-    private boolean beforeMidnight(int hour) {
-        return hour < 12;
-    }
-
-    private boolean beforeBedtime(int hour) {
-        return hour >= this.bedTime;
+    private boolean before(int hour, int time) {
+        return hour < time;
     }
 
     public int calculate(final int startTime, final int endTime) {
@@ -48,7 +41,7 @@ public class BabySitterPaymentCalculator {
         int adjustedEndTime = adjustTime(endTime);
 
         int pay = 0;
-        for (int i = adjustedStartTime; i < adjustedEndTime; i++) {
+        for (int i = adjustedStartTime; before(i, adjustedEndTime); i++) {
             pay += hourlyRates.get(i);
         }
         return pay;
